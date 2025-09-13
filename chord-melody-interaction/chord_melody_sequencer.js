93a440c2145151fc448c192e8478693621b018ce
Steve Meyer <steve.e.meyer@gmail.com>
2025-09-13 13:58:06 -0500

    Code cleanup: make properties and methods private

--- chord-melody-interaction/chord_melody_sequencer.js
+++ chord-melody-interaction/chord_melody_sequencer.js
@@ -7,25 +7,23 @@ import { Configuration } from "./src/configuration.js";
 
 export class ChordMelodySequencer {
   #configuration;
-  chords;
-  accompaniment;
-  beat;
-  channel;
-  maxCount;
-  count;
+  #chordVoice;
+  #accompanimentVoice;
+  #beatVoice;
+  #voiceMessageChannel;
+  #iterationCount;
 
 
   constructor(configFilepath) {
     this.#configuration = new Configuration(configFilepath);
 
-    const midiPort     = new MidiOutput("tblswvs.out", true);
-    this.chords        = new Voice(midiPort, 1);
-    this.accompaniment = new Voice(midiPort, 2);
-    this.beat          = new Voice(midiPort, 10);
+    const midiPort           = new MidiOutput("tblswvs.out", true);
+    this.#chordVoice         = new Voice(midiPort, 1);
+    this.#accompanimentVoice = new Voice(midiPort, 2);
+    this.#beatVoice          = new Voice(midiPort, 10);
 
-    this.channel  = new MessageChannel();
-    this.maxCount = this.#configuration.iterations;
-    this.count    = 0;
+    this.#voiceMessageChannel = new MessageChannel();
+    this.#iterationCount      = 0;
 
     this.#configureChannelPorts();
     this.#printConfiguration();
@@ -34,7 +32,7 @@ export class ChordMelodySequencer {
 
   run() {
     // Start the sequencer by posting the scale degree 1 to the chords voice.
-    this.channel.port2.postMessage(1);
+    this.#voiceMessageChannel.port2.postMessage(1);
   }
 
 
@@ -46,40 +44,40 @@ export class ChordMelodySequencer {
   }
 
 
-  randomChord(root) {
+  #randomChord(root) {
     return this.#configuration.key.chord(
-      root === undefined ? this.#configuration.key.degree(this.randomScaleDegree()).scaleDegree : root,
+      root === undefined ? this.#configuration.key.degree(this.#randomScaleDegree()).scaleDegree : root,
       this.#configuration.chordTypes[Math.floor(Math.random() * this.#configuration.chordTypes.length)]
     );
   }
 
 
-  randomKick() {
+  #randomKick() {
     return this.#configuration.drumPads.kicks[Math.floor(Math.random() * this.#configuration.drumPads.kicks.length)];
   }
 
 
-  randomSnare() {
+  #randomSnare() {
     return this.#configuration.drumPads.snares[Math.floor(Math.random() * this.#configuration.drumPads.snares.length)];
   }
 
 
-  randomPerc() {
+  #randomPerc() {
     return this.#configuration.drumPads.percs[Math.floor(Math.random() * this.#configuration.drumPads.percs.length)];
   }
 
 
-  randomDuration() {
+  #randomDuration() {
     return this.#configuration.durations[Math.floor(Math.random() * this.#configuration.durations.length)];
   }
 
 
-  randomScaleDegree() {
+  #randomScaleDegree() {
     return this.#configuration.scaleDegrees[Math.floor(Math.random() * this.#configuration.scaleDegrees.length)];
   }
 
 
-  midiToScaleDegree(midiNoteNumber) {
+  #midiToScaleDegree(midiNoteNumber) {
     const scaleOffsetIndex = this.#configuration.key.mode.scaleOffsets.indexOf(
       midiNoteNumber % 12 - this.#configuration.key.midiTonic
     );
@@ -87,7 +85,7 @@ export class ChordMelodySequencer {
   }
 
 
-  logEvent(count, chord, note, duration) {
+  #logEvent(count, chord, note, duration) {
     console.log(
       ` ${count.toString().padStart(4)}.`,
       "chord:", (`${chord.root}${chord.quality} ` +
@@ -103,40 +101,40 @@ export class ChordMelodySequencer {
      * Port 1 represents the chord progression. Choose a random duration and chord,
      * then tell Port 2 what you played so it can accompany.
      */
-    this.channel.port1.on("message", (chordRoot) => {
-      this.count++;
+    this.#voiceMessageChannel.port1.on("message", (chordRoot) => {
+      this.#iterationCount++;
 
-      if (this.count > this.maxCount) {
+      if (this.#iterationCount > this.#configuration.iterations) {
         setTimeout(() => {
-          this.channel.port1.close();
-          this.channel.port2.close();
+          this.#voiceMessageChannel.port1.close();
+          this.#voiceMessageChannel.port2.close();
         }, 3000);
 
         return;
       }
 
-      const duration = this.count < 5 || this.count > this.maxCount - 4 ? 1500 : this.randomDuration();
-      const chord    = this.randomChord(chordRoot);
+      const duration = this.#iterationCount < 5 || this.#iterationCount > this.#configuration.iterations - 4 ? 1500 : this.#randomDuration();
+      const chord    = this.#randomChord(chordRoot);
 
-      this.beat.playNote(this.randomKick(), 100);
-      chord.midi.forEach(note => this.chords.playNote(note, duration));
+      this.#beatVoice.playNote(this.#randomKick(), 100);
+      chord.midi.forEach(note => this.#chordVoice.playNote(note, duration));
 
-      this.channel.port1.postMessage({chord: chord, duration: duration});
+      this.#voiceMessageChannel.port1.postMessage({chord: chord, duration: duration});
     });
 
     /**
      * Port 2 represents the accompanying melody. Based on the chord played, choose
      * a random chord note to play in another voice.
      */
-    this.channel.port2.on("message", (data) => {
+    this.#voiceMessageChannel.port2.on("message", (data) => {
       // Decide whether or not to play a melodic note. If yes, choose one of the chord notes.
-      const note = this.count >= 5 && Math.random() < 0.7 ?
+      const note = this.#iterationCount >= 5 && Math.random() < 0.7 ?
                    data.chord.midi[Math.floor(Math.random() * data.chord.midi.length)] :
                    undefined;
 
       if (data.duration >= 500) {
         setTimeout(() => {
-          this.beat.playNote(Math.random() > 0.5 ? this.randomPerc() : this.randomSnare(), 100);
+          this.#beatVoice.playNote(Math.random() > 0.5 ? this.#randomPerc() : this.#randomSnare(), 100);
         }, data.duration / 4);
       }
 
@@ -145,17 +143,17 @@ export class ChordMelodySequencer {
       if (note !== undefined)
         setTimeout(() => {
           if (data.duration >= 250)
-            this.beat.playNote(Math.random() > 0.5 ? this.randomPerc() : this.randomSnare(), 100);
+            this.#beatVoice.playNote(Math.random() > 0.5 ? this.#randomPerc() : this.#randomSnare(), 100);
 
-          this.accompaniment.playNote(note, data.duration);
+          this.#accompanimentVoice.playNote(note, data.duration);
         }, data.duration / 2);
 
       // After the current duration wraps up, tell Port 1 what note you chose (which may
       // be undefined) so it can use it when choosing the next chord to play.
-      setTimeout(() => this.channel.port2.postMessage(this.midiToScaleDegree(note)), data.duration);
+      setTimeout(() => this.#voiceMessageChannel.port2.postMessage(this.#midiToScaleDegree(note)), data.duration);
 
       // Finally log the chord, accompaniment note and duration for the current cycle iteration.
-      this.logEvent(this.count, data.chord, note, data.duration);
+      this.#logEvent(this.#iterationCount, data.chord, note, data.duration);
     });
   }
 }
